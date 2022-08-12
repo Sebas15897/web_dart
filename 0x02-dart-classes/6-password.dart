bd5a26e68b1e296345775fc206bf0c0aefed9c82
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2022-08-11 23:33:51 -0500

    Update 6-password.dart

--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,26 +1,33 @@
 class Password {
-  String? _password = "";
+  String _password = '';
 
-  String get password => _password!;
+  Password({password: String}) {
+    this._password = password;
+  }
 
-  set password(String password) => _password = password;
-  Password({password}) : _password = password;
+  String get password {
+    return this._password;
+  }
+
+  void set password(String password) {
+    this._password = password;
+  }
 
   bool isValid() {
-    if ((this._password?.length ?? 0) > 6 &&
-        (this._password?.length ?? 0) < 18) {
-      if (this._password!.contains(RegExp(r"(?=.*[a-z])(?=.*[A-Z])\w+"))) {
-        if (this._password!.contains(RegExp(r'\d'))) {
-
+    if (this._password.length >= 8 && this._password.length <= 16) {
+      if (this._password.contains(RegExp(r'[A-Z]')) &&
+          this._password.contains(RegExp(r'[a-z]'))) {
+        if (this._password.contains(RegExp(r'[0-9]'))) {
           return true;
         }
       }
     }
+
     return false;
   }
 
   @override
   String toString() {
-    return "Your Password is: ${this._password}";
+    return 'Your Password is: ${this._password}';
   }
 }
